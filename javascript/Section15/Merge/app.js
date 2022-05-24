7dd824ef2aff534439bf36c4ed8f492de864e08d
Dae Hoon Kim <hunimoda@gmail.com>
2022-05-24 16:21:55 +0900

    Section15: merge sort - my version of code

--- javascript/Section15/Merge/app.js
+++ javascript/Section15/Merge/app.js
@@ -1,6 +1,6 @@
 function merge(arr1, arr2) {
-	let i = 0, j = 0;
 	const merged = [];
+	let i = 0, j = 0;
 	while (i < arr1.length && j < arr2.length) {
 		if (arr1[i] < arr2[j]) merged.push(arr1[i++]);
 		else merged.push(arr2[j++]);
@@ -8,3 +8,11 @@ function merge(arr1, arr2) {
 	merged.push(...arr1.slice(i), ...arr2.slice(j));
 	return merged;
 }
+
+function mergeSort(arr) {
+	if (arr.length <= 1) return [...arr];
+	return merge(mergeSort(arr.slice(0, arr.length / 2)),
+							 mergeSort(arr.slice(arr.length / 2)));
+}
+
+console.log(mergeSort([6, 9, 10, -3, 8, 5, -9, 2], [13, 18, 3, -5, -4, 1, 0, 7]));
