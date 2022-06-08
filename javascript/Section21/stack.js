fa34378274d3d816f2afb0b8c98d0a3d6edd6aa4
Dae Hoon Kim <hunimoda@gmail.com>
2022-06-09 00:33:57 +0900

    Section21: list-based stack

--- javascript/Section21/stack.js
+++ javascript/Section21/stack.js
@@ -1,30 +1,75 @@
-// Array-based Stack
+// // Array-based Stack
+// class Stack {
+//   constructor() {
+//     this.array = [];
+//   }
+
+//   push(value) {
+//     this.array.push(value);
+//   }
+
+//   pop() {
+//     if (this.empty()) return;
+//     return this.array.pop();
+//   }
+
+//   top() {
+//     if (this.empty()) return;
+//     return this.array[this.array.length - 1];
+//   }
+
+//   empty() {
+//     return this.array.length === 0;
+//   }
+// }
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
 class Stack {
   constructor() {
-    this.array = [];
+    this.head = null;
+    this.size = 0;
   }
 
   push(value) {
-    this.array.push(value);
+    const newNode = new Node(value, this.head);
+    this.head = newNode;
+    return ++this.size;
   }
 
   pop() {
     if (this.empty()) return;
-    return this.array.pop();
+    const popNode = this.head;
+    this.head = popNode.next;
+    this.size--;
+    return popNode.value;
   }
 
   top() {
-    if (this.empty()) return;
-    return this.array[this.array.length - 1];
+    return this.head?.value;
   }
 
   empty() {
-    return this.array.length === 0;
+    return !this.head;
   }
 }
 
 const stack = new Stack();
-[1, 2, 3, 4, 5].forEach(elem => stack.push(elem));
+console.log(`(after initialization) empty: ${stack.empty()}`);
+console.log(`(after initialization) pop: ${stack.pop()}`);
+console.log(`(after initialization) top: ${stack.top()}`);
+
+console.log("pushing data...");
+[1, 2, 3, 4, 5].forEach(elem => {
+  console.log(`pushing data ${elem}... ${stack.push(elem)}`);
+});
+
+console.log("pop!");
 console.log(stack.pop());
 console.log(stack.pop());
 console.log(stack.pop());
